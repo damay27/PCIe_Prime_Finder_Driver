7c232261d7a675ab2a211e142f375601b4869dd2
daniel.may <damay27@Outlook.com>
2021-01-14 16:30:50 +0000

    Added comments to the driver build/load script.

--- driver_build_load.sh
+++ driver_build_load.sh
@@ -5,10 +5,14 @@ DEVICE_FILE_NAME=prime_finder
 
 ./device_refresh.sh
 
+#Remove the driver if it is already loaded
 rmmod $DRIVER_NAME
+#Rebuild the driver
 make
+#Reload the driver once the build has finished
 insmod $DRIVER_NAME.ko
 
 MAJOR_NUMBER=`cat /proc/devices | grep $DRIVER_NAME | awk '{print $1}'`
 
+#The c argument creates a character device
 mknod /dev/$DEVICE_FILE_NAME c $MAJOR_NUMBER 0
